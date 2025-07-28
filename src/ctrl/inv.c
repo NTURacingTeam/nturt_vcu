711c625e6d61021519368efbfe5aca80df438f90
quantumspawner <jet22854111@gmail.com>
2025-07-28 11:44:53 +0800

    hot fixes from fst

--- src/ctrl/inv.c
+++ src/ctrl/inv.c
@@ -28,13 +28,23 @@ LOG_MODULE_REGISTER(vcu_ctrl_inv);
 
 #define CTRL_RESET_BIT_HOLD_TIME K_MSEC(100)
 
+#define INV_ERR_THRES 10
+#define INV_ERR_DEC_TIME K_MINUTES(1)
+
 /* type ----------------------------------------------------------------------*/
+struct fault_dec_work {
+  struct k_work_delayable work;
+  int inv_index;
+};
+
 struct ctrl_inv_ctx {
   struct k_work_delayable fault_reset_work;
+  struct fault_dec_work fault_dec_work[4][INV_ERR_THRES];
 
   struct k_mutex lock;
 
   struct msg_ctrl_word msg;
+  int fault_counts[4];
 };
 
 /* static function declaration -----------------------------------------------*/
@@ -45,11 +55,20 @@ static void msg_cb(const struct zbus_channel *chan);
 static void err_cb(uint32_t errcode, bool set, void *user_data);
 static void states_cb(enum states_state state, bool is_entry, void *user_data);
 static void fault_reset_work(struct k_work *work);
+static void fault_dec_work(struct k_work *work);
 
 /* static variable -----------------------------------------------------------*/
+#define FAULT_DEC_WORK_INITIALIZER(i, _)                           \
+  [i] = {[0 ... INV_ERR_THRES - 1] = {                             \
+             .work = Z_WORK_DELAYABLE_INITIALIZER(fault_dec_work), \
+             .inv_index = i,                                       \
+         }}
+
 static struct ctrl_inv_ctx g_ctx = {
     .fault_reset_work = Z_WORK_DELAYABLE_INITIALIZER(fault_reset_work),
+    .fault_dec_work = {LISTIFY(4, FAULT_DEC_WORK_INITIALIZER, (, ))},
     .lock = Z_MUTEX_INITIALIZER(g_ctx.lock),
+    .fault_counts = {0},
 };
 
 ZBUS_LISTENER_DEFINE(ctrl_inv_listener, msg_cb);
@@ -70,7 +89,8 @@ ERR_DEFINE(inv_rr_no_power, ERR_CODE_INV_RR_HV_LOW, ERR_SEV_WARN,
            "Inverter RR HV low voltage");
 
 ERR_CALLBACK_DEFINE(err_cb, NULL,
-                    ERR_FILTER_CODE(ERR_CODE_INV_FL_HV_LOW, ERR_CODE_INV_FR_HV_LOW,
+                    ERR_FILTER_CODE(ERR_CODE_INV_FL_HV_LOW,
+                                    ERR_CODE_INV_FR_HV_LOW,
                                     ERR_CODE_INV_RL_HV_LOW,
                                     ERR_CODE_INV_RR_HV_LOW));
 
@@ -83,8 +103,14 @@ int ctrl_inv_fault_reset() {
     return -EBUSY;
   }
 
-  ctrl_inv_word_set_and_pub(&g_ctx, CTRL_WORD_FAULT_RESET, true);
+  memset(g_ctx.fault_counts, 0, sizeof(g_ctx.fault_counts));
+  for (int i = 0; i < 4; i++) {
+    for (int j = 0; j < INV_ERR_THRES; j++) {
+      k_work_cancel_delayable(&g_ctx.fault_dec_work[i][j].work);
+    }
+  }
 
+  ctrl_inv_word_set_and_pub(&g_ctx, CTRL_WORD_FAULT_RESET, true);
   k_work_reschedule(&g_ctx.fault_reset_work, CTRL_RESET_BIT_HOLD_TIME);
 
   return 0;
@@ -118,8 +144,32 @@ static void msg_cb(const struct zbus_channel *chan) {
 
   for (int i = 2; i < 4; i++) {
     if (!err_is_set(ERR_CODE_HB_INV_FL + i)) {
-      err_report(ERR_CODE_INV_FL + i,
-                 msg->status.values[i] & STATUS_WORD_FAULT);
+      if (msg->status.values[i] & STATUS_WORD_FAULT) {
+        if (err_is_set(ERR_CODE_INV_FL + i) ||
+            g_ctx.fault_counts[i] >= INV_ERR_THRES) {
+          err_report(ERR_CODE_INV_FL + i, true);
+
+        } else {
+          g_ctx.fault_counts[i]++;
+          ctrl_inv_word_set_and_pub(&g_ctx, CTRL_WORD_FAULT_RESET, true);
+          k_work_reschedule(&g_ctx.fault_reset_work, CTRL_RESET_BIT_HOLD_TIME);
+
+          for (int j = 0; j < INV_ERR_THRES; j++) {
+            if (!k_work_delayable_is_pending(
+                    &g_ctx.fault_dec_work[i][j].work)) {
+              k_work_schedule(&g_ctx.fault_dec_work[i][j].work,
+                              INV_ERR_DEC_TIME);
+              break;
+            }
+          }
+
+          LOG_WRN("Inverter %d fault detected, count increased to %d", i,
+                  g_ctx.fault_counts[i]);
+        }
+      } else {
+        err_report(ERR_CODE_INV_FL + i, false);
+      }
+
       err_report(ERR_CODE_INV_FL_HV_LOW + i,
                  !(msg->status.values[i] & STATUS_WORD_POWER));
     }
@@ -149,4 +199,13 @@ static void fault_reset_work(struct k_work *work) {
       CONTAINER_OF(dwork, struct ctrl_inv_ctx, fault_reset_work);
 
   ctrl_inv_word_set_and_pub(ctx, CTRL_WORD_FAULT_RESET, false);
+}
+
+static void fault_dec_work(struct k_work *work) {
+  struct k_work_delayable *dwork = k_work_delayable_from_work(work);
+  struct fault_dec_work *dec = CONTAINER_OF(dwork, struct fault_dec_work, work);
+
+  g_ctx.fault_counts[dec->inv_index]--;
+  LOG_INF("Decremented fault count for inverter %d to %d", dec->inv_index,
+          g_ctx.fault_counts[dec->inv_index]);
 }
