001e7052d7056c9575dac1872016593a3a3ac663
quantumspawner <jet22854111@gmail.com>
2025-08-15 19:51:41 +0800

    revert inverter fault reset

--- src/ctrl/inv.c
+++ src/ctrl/inv.c
@@ -25,32 +25,18 @@ LOG_MODULE_REGISTER(vcu_ctrl_inv);
 #define STATUS_WORD_FAULT BIT(3)
 #define STATUS_WORD_POWER BIT(4)
 
-#define STATUS_WORD_FAULT_TYPE GENMASK(15, 8)
-#define STATUS_WORD_FAULT_OC (1 << 8)
-
 #define CTRL_WORD_ENABLE BIT(3)
 #define CTRL_WORD_FAULT_RESET BIT(5)
 
 #define CTRL_RESET_BIT_HOLD_TIME K_MSEC(100)
 
-#define INV_ERR_THRES 10
-#define INV_ERR_DEC_TIME K_MINUTES(1)
-#define INV_DISABLE_BRAKE_THRES 10.0F
-
 /* type ----------------------------------------------------------------------*/
-struct fault_dec_work {
-  struct k_work_delayable work;
-  int inv_index;
-};
-
 struct ctrl_inv_ctx {
   struct k_work_delayable fault_reset_work;
-  struct fault_dec_work fault_dec_work[4][INV_ERR_THRES];
 
   struct k_mutex lock;
 
   struct msg_ctrl_word msg;
-  int fault_counts[4];
 };
 
 /* static function declaration -----------------------------------------------*/
@@ -61,26 +47,16 @@ static void msg_cb(const struct zbus_channel *chan);
 static void err_cb(uint32_t errcode, bool set, void *user_data);
 static void states_cb(enum states_state state, bool is_entry, void *user_data);
 static void fault_reset_work(struct k_work *work);
-static void fault_dec_work(struct k_work *work);
 
 /* static variable -----------------------------------------------------------*/
-#define FAULT_DEC_WORK_INITIALIZER(i, _)                           \
-  [i] = {[0 ... INV_ERR_THRES - 1] = {                             \
-             .work = Z_WORK_DELAYABLE_INITIALIZER(fault_dec_work), \
-             .inv_index = i,                                       \
-         }}
-
 static const struct device *can = DEVICE_DT_GET(DT_CHOSEN(zephyr_canbus));
 
 static struct ctrl_inv_ctx g_ctx = {
     .fault_reset_work = Z_WORK_DELAYABLE_INITIALIZER(fault_reset_work),
-    .fault_dec_work = {LISTIFY(4, FAULT_DEC_WORK_INITIALIZER, (, ))},
     .lock = Z_MUTEX_INITIALIZER(g_ctx.lock),
-    .fault_counts = {0},
 };
 
 ZBUS_LISTENER_DEFINE(ctrl_inv_listener, msg_cb);
-ZBUS_CHAN_ADD_OBS(msg_sensor_cockpit_chan, ctrl_inv_listener, 0);
 ZBUS_CHAN_ADD_OBS(msg_ts_inv_chan, ctrl_inv_listener, 0);
 
 ERR_DEFINE(inv_fl, ERR_CODE_INV_FL, ERR_SEV_FATAL, "Inverter FL error");
@@ -110,13 +86,6 @@ int ctrl_inv_fault_reset() {
   if (states_get() & STATE_RUNNING) {
     LOG_ERR("Cannot reset inverter fault while running");
     return -EBUSY;
-  }
-
-  memset(g_ctx.fault_counts, 0, sizeof(g_ctx.fault_counts));
-  for (int i = 0; i < 4; i++) {
-    for (int j = 0; j < INV_ERR_THRES; j++) {
-      k_work_cancel_delayable(&g_ctx.fault_dec_work[i][j].work);
-    }
   }
 
   ctrl_inv_word_set_and_pub(&g_ctx, CTRL_WORD_FAULT_RESET, true);
@@ -150,52 +119,14 @@ static void ctrl_inv_word_set_and_pub(struct ctrl_inv_ctx *ctx, uint16_t flags,
 }
 
 static void msg_cb(const struct zbus_channel *chan) {
-  if (chan == &msg_sensor_cockpit_chan) {
-    const struct msg_sensor_cockpit *msg = zbus_chan_const_msg(chan);
+  const struct msg_ts_inv *msg = zbus_chan_const_msg(chan);
 
-    // if (states_get() & STATE_RUNNING) {
-    //   ctrl_inv_word_set_and_pub(&g_ctx, CTRL_WORD_ENABLE,
-    //                             !(msg->brake > INV_DISABLE_BRAKE_THRES));
-    // }
-
-  } else if (chan == &msg_ts_inv_chan) {
-    const struct msg_ts_inv *msg = zbus_chan_const_msg(chan);
-
-    for (int i = 2; i < 4; i++) {
-      if (!err_is_set(ERR_CODE_HB_INV_FL + i)) {
-        if (msg->status.values[i] & STATUS_WORD_FAULT) {
-          if (!err_is_set(ERR_CODE_INV_FL + i) &&
-              (msg->status.values[i] & STATUS_WORD_FAULT_TYPE) ==
-                  STATUS_WORD_FAULT_OC &&
-              g_ctx.fault_counts[i] < INV_ERR_THRES) {
-            g_ctx.fault_counts[i]++;
-            ctrl_inv_word_set_and_pub(&g_ctx, CTRL_WORD_FAULT_RESET, true);
-            sys_work_reschedule(&g_ctx.fault_reset_work,
-                              CTRL_RESET_BIT_HOLD_TIME);
-
-            for (int j = 0; j < INV_ERR_THRES; j++) {
-              if (!k_work_delayable_is_pending(
-                      &g_ctx.fault_dec_work[i][j].work)) {
-                sys_work_schedule(&g_ctx.fault_dec_work[i][j].work,
-                                INV_ERR_DEC_TIME);
-                break;
-              }
-            }
-
-            LOG_WRN("Inverter %d OC detected, count increased to %d", i,
-                    g_ctx.fault_counts[i]);
-
-          } else {
-            err_report(ERR_CODE_INV_FL + i, true);
-          }
-
-        } else {
-          err_report(ERR_CODE_INV_FL + i, false);
-        }
-
-        err_report(ERR_CODE_INV_FL_HV_LOW + i,
-                   !(msg->status.values[i] & STATUS_WORD_POWER));
-      }
+  for (int i = 2; i < 4; i++) {
+    if (!err_is_set(ERR_CODE_HB_INV_FL + i)) {
+      err_report(ERR_CODE_INV_FL + i,
+                 msg->status.values[i] & STATUS_WORD_FAULT);
+      err_report(ERR_CODE_INV_FL_HV_LOW + i,
+                 !(msg->status.values[i] & STATUS_WORD_POWER));
     }
   }
 }
@@ -238,13 +169,4 @@ static void fault_reset_work(struct k_work *work) {
       CONTAINER_OF(dwork, struct ctrl_inv_ctx, fault_reset_work);
 
   ctrl_inv_word_set_and_pub(ctx, CTRL_WORD_FAULT_RESET, false);
-}
-
-static void fault_dec_work(struct k_work *work) {
-  struct k_work_delayable *dwork = k_work_delayable_from_work(work);
-  struct fault_dec_work *dec = CONTAINER_OF(dwork, struct fault_dec_work, work);
-
-  g_ctx.fault_counts[dec->inv_index]--;
-  LOG_INF("Decremented fault count for inverter %d to %d", dec->inv_index,
-          g_ctx.fault_counts[dec->inv_index]);
 }
