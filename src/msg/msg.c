884c444fb714b1f5ef3263ac49897f2f4726ec1e
quantumspawner <jet22854111@gmail.com>
2026-06-17 00:37:14 +0800

    msg: bug fix

--- src/msg/msg.c
+++ src/msg/msg.c
@@ -17,9 +17,14 @@ MSG_AGG_TO_MSG_DEFINE(msg_sensor_wheel_agg,
     msg_sensor_wheel, AGG_DATA_INIT(0),
     K_MSEC(10), K_MSEC(8), K_MSEC(3), 0,
 
-    AGG_MEMBER(speed.fl), AGG_MEMBER(speed.fr), AGG_MEMBER(speed.rl), AGG_MEMBER(speed.rr),
+    AGG_MEMBER(speed.fl), AGG_MEMBER(speed.fr),
+    AGG_MEMBER(speed.rl), AGG_MEMBER(speed.rr),
+
+    AGG_MEMBER(torque.fl), AGG_MEMBER(torque.fr),
     AGG_MEMBER(torque.rl), AGG_MEMBER(torque.rr),
-    AGG_MEMBER(susp_travel.fl), AGG_MEMBER(susp_travel.fr), AGG_MEMBER(susp_travel.rl), AGG_MEMBER(susp_travel.rr)
+
+    AGG_MEMBER(susp_travel.fl), AGG_MEMBER(susp_travel.fr),
+    AGG_MEMBER(susp_travel.rl), AGG_MEMBER(susp_travel.rr)
 );
 
 // clang-format on
